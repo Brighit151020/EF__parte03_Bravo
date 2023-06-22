65f51c5f93da6031073952181ef8fb5849cf07b7
Brighit Bravo Ames <antonellab0503@gmail.com>
2023-06-22 16:54:22 -0500

    UC3-Parte03-BravoAmes

--- EC3_Montesinos_Matta/EC3_Montesinos_Matta/urls.py
+++ EC3_Montesinos_Matta/EC3_Montesinos_Matta/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('inicio/', views.index, name="inicio"),
+    path('rango/',views.rango, name="rango"),
 ]
--- EC3_Montesinos_Matta/miapp/views.py
+++ EC3_Montesinos_Matta/miapp/views.py
@@ -15,3 +15,27 @@ def index(request):
         'mensaje':'Proyecto web con Django',
         'cursos':curso
 })
+
+
+def rango(request):
+    a = 5
+    b = 100
+    
+    rango_numeros = []
+    
+    for num in range(a, b+1):
+        if num > 1:  # Los números primos son mayores que 1
+            es_primo = True
+            for i in range(2, num):
+                if num % i == 0:
+                    es_primo = False
+                    break
+            if es_primo:
+                rango_numeros.append(num)
+    
+    return render(request, 'rango.html', {
+        'titulo': 'Rango',
+        'a': a,
+        'b': b,
+        'rango_numeros': rango_numeros
+    })
